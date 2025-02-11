f50d8ccff262436be17770d8ac27b07521047105
Jonathan Lee <thenameless0n3@gmail.com>
2025-02-10 20:29:45 -0800

    cleaned up app.ts, updated readme
    Signed-off-by: Jonathan Lee <thenameless0n3@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -49,43 +49,41 @@ async function getRiotGamesListings(browser: Browser, hashes: Set<string>, filte
             if (filters.locationIncludes && !filters.locationIncludes.some((term) => jobLocation.includes(term))) continue;
             if (filters.locationExcludes && filters.locationExcludes.some((term) => jobLocation.includes(term))) continue;
             jobListings.push({ id: hashId, jobTitle: jobTitle, jobLocation: jobLocation, jobUrl: jobUrl, foundDate: foundDate });
-    }
-}
-page.close();
-return jobListings;
-}
-
-async function getCaesarsListings(browser: Browser, filters: Filters) {
-    const jobListings = [];
-
-    const page = await browser.newPage();
-    await page.goto("https://edmn.fa.us2.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/requisitions?lastSelectedFacet=CATEGORIES&selectedCategoriesFacet=300000289546439%3B300000830240017");
-
-    const rawJobListings = await page.$$('job-grid-item__link'); // Using $$ for multiple elements
-
-    for (const rawJobListing of rawJobListings) {
-        const jobTitle = await rawJobListing.$eval('div.job-row__col--primary', (el: { textContent: any; }) => el.textContent) || "";
-
-        if (
-            filters.titleIncludes.some((term: any) => jobTitle.includes(term)) &&
-            !filters.titleExcludes.some((term: any) => jobTitle.includes(term))
-        ) {
-
-            const jobLocation = await rawJobListing.$$eval('div.job-row__col--secondary', (els: { textContent: any; }[]) => els[2].textContent);
-            const jobUrl = await rawJobListing.evaluate(el => (el as HTMLAnchorElement).href);
-            const foundDate = moment().format('YYYY-MM-DD HH:mm:ss');
-
-            jobListings.push({ jobTitle, jobLocation, jobUrl, foundDate });
         }
     }
-
+    page.close();
     return jobListings;
 }
 
-// let filters: Filters = {
-//     titleIncludes: ["Software", "Developer"],
-//     titleExcludes: ["Senior", "Principal", "Manager", "Staff", "Lead", "Distinguished", "Contractor"]
-// }
+async function getCaesarsListings(browser: Browser, hashes: Set<string>, filters: Filters): Promise<JobListing[]> {
+    // console.log("Starting scraping Riot Games career page");
+    const jobListings: JobListing[] = [];
+
+    let rawJobListingsXpath = "//ul[@class='job-list__body list--unstyled']/li/a";
+
+    const page = await browser.newPage();
+    await page.goto("https://edmn.fa.us2.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/requisitions?lastSelectedFacet=CATEGORIES&selectedCategoriesFacet=300000289546439%3B300000830240017", { waitUntil: 'domcontentloaded' });
+    // console.log(`Page loaded in ${(await page.metrics()).TaskDuration} seconds`)
+
+    let rawJobListings = await page.$$(`::-p-xpath(${rawJobListingsXpath})`); // Using $$ for multiple elements
+    for (const rawJobListing of rawJobListings) {
+        const jobTitle = await rawJobListing.$eval('div.job-row__col--primary', el => el.textContent) || "n/a";
+        const jobLocation = await rawJobListing.$$eval('div.job-row__col--secondary', els => els[2].textContent) || "n/a";
+        const jobUrl = await rawJobListing.getProperty('href').then(el => el?.jsonValue()) as string || "n/a";
+        const foundDate = moment().format('YYYY-MM-DD HH:mm:ss');
+        const hashId: string = "RIOT-" + new URL(jobUrl).pathname.split('/').pop();
+        if (!hashes.has(hashId)) {
+            hashes.add(hashId);
+            if (filters.titleIncludes && !filters.titleIncludes.some((term) => jobTitle.includes(term))) continue;
+            if (filters.titleExcludes && filters.titleExcludes.some((term) => jobTitle.includes(term))) continue;
+            if (filters.locationIncludes && !filters.locationIncludes.some((term) => jobLocation.includes(term))) continue;
+            if (filters.locationExcludes && filters.locationExcludes.some((term) => jobLocation.includes(term))) continue;
+            jobListings.push({ id: hashId, jobTitle: jobTitle, jobLocation: jobLocation, jobUrl: jobUrl, foundDate: foundDate });
+        }
+    }
+    page.close();
+    return jobListings;
+}
 
 async function main() {
     let filters: Filters = {
@@ -107,12 +105,6 @@ async function main() {
             });
             if (newListings.length > 0) {
                 for (const listing of newListings) {
-                    // console.log(listing.id);
-                    // console.log(listing.jobTitle);
-                    // console.log(listing.jobLocation);
-                    // console.log(listing.jobUrl);
-                    // console.log(`Found on: ${listing.foundDate}`);
-                    // console.log("");
                     fetch('https://ntfy.sh/joeyeyey-job-scraper', {
                         method: 'POST',
                         body: `${listing.jobTitle}\n${listing.jobLocation}\n${listing.jobUrl}`
